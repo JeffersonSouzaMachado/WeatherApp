da380d275e67439c4024f91bb81b90c3c8c8f697
Jefferson Machado <jefferson.machado@gmail.com>
2024-04-28 12:05:32 -0300

    create constraints with refeer

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(
+            "Load at first time"
+        )
+        
+        setupView()
+        
     }
-
-
+    
+    
+    private func setupView(){
+        view.backgroundColor = .red
+        
+        
+        let customView = UIView(frame: .zero)
+        customView.translatesAutoresizingMaskIntoConstraints = false
+        customView.backgroundColor = .yellow
+     
+        view.addSubview(
+            customView
+        )
+        
+        NSLayoutConstraint.activate([
+            customView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            customView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
+            customView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
+            customView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100),
+        ])
+        
+       
+    }
+    
+    
+    
+    
+    
+    
+    
 }
+
 
