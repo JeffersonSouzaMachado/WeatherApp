480a8a78a03c91c9769e158424a55ad9a180219f
Jefferson Machado <jefferson.machado@gmail.com>
2024-04-29 09:50:07 -0300

    insert temperature cells, create color extensions

---
+++ Color+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Color+Extensions.swift
+//  WeatherApp
+//
+//  Created by Jefferson Machado on 29/04/24.
+//
+
+import Foundation
---
+++ HourlyForecastCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  HourlyForecastCollectionViewCell.swift
+//  WeatherApp
+//
+//  Created by Jefferson Machado on 28/04/24.
+//
+
+import UIKit
+
+class HourlyForecastCollectionViewCell: UICollectionViewCell {
+    
+}
